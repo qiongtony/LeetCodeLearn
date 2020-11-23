58959b1a41b389d3f438229b9e709c416ce92b4c
wuweishan1995 <wws@gzqianyin.cn>
2020-11-23 10:22:05 +0800

    *)实现堆的插入和删除

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -6,39 +6,40 @@ public class QuickSort implements Sort{
         if (a == null || a.length <= 1){
             return 0;
         }
-        quick(a, 0, a.length - 1);
+        quickSort(a, 0, a.length - 1);
         return 0;
     }
 
-    public void quick(int[] a, int left, int right){
+    public void quickSort(int[]a,int left, int right){
         if (left >= right){
             return;
         }
-        int q = partition(a, left, right);
-        quick(a, left, q - 1);
-        quick(a, q, right);
-
+        int p = partition(a, left, right);
+        quickSort(a, left, p - 1);
+        quickSort(a, p + 1, right);
     }
 
-    public int partition(int[] a,int left, int right){
-        int pivot = a[right];
+    private int partition(int[]a, int left, int right){
         int i = left;
+        int pivot = a[right];
         // 将a[left..right]分成三部分，a[left..i-1]小于哨兵，a[i+1..right]大于哨兵值
-        for (int j = left; j < right; j++){
+        for (int j = left;j < right;j++){
             if (a[j] < pivot){
                 if (i == j){
                     i++;
                 }else{
-                    // 交换值
-                    int tmp = a[i];
-                    a[i++] = a[j];
-                    a[j] = tmp;
+                    swap(a, i, j);
+                    i++;
                 }
             }
         }
-        int tmp = a[i];
-        a[i] = a[right];
-        a[right] = tmp;
+        swap(a, i, right);
         return i;
+    }
+
+    private void swap(int[] a, int left, int right){
+        int tmp = a[left];
+        a[left] = a[right];
+        a[right] = tmp;
     }
 }
